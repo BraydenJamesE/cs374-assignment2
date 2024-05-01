a78d5cf1be63fda281871cc99b1143fb597d230d
Brayden Edwards <edwabray@oregonstate.edu>
2024-04-30 21:07:58 -0700

    Generating directory name added

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 
 
@@ -78,27 +79,47 @@ char* getLargestFileName() {
     char* largestFileName = malloc(sizeof(char) * 255); // 255 is the max file name length on mac.
     int largestFileSize = 0;
     while ((entry = readdir(dir)) != NULL) {
-        if (strncmp(entry->d_name, "movies_", strlen("movies_")) == 0) { // checking files that have the required prefix "movies_"
+        if (strncmp(entry->d_name, "movies_", strlen("movies_")) == 0
+        && strcmp(entry->d_name + strlen(entry->d_name) - strlen(".csv"), ".csv") == 0) { // checking files that have the required prefix "movies_" and have the extension ".csv"
             int currentFileSize = getFileSize(entry->d_name); // getting the current file size
             if (currentFileSize > largestFileSize) { // if the current file size is greater than the largest file so far, update the largest file to the current file
                 largestFileSize = currentFileSize;
                 strcpy(largestFileName, entry->d_name); // copy the current file name to the largest file name string
             }
-            else if (currentFileSize == largestFileSize
-                && strcmp(entry->d_name + strlen(entry->d_name) - strlen(".csv"), ".csv") == 0) { // if the largest file size is the same as the current file size and the current file ends in ".csv", update the current file to the largest file
-                strcpy(largestFileName, entry->d_name); // copy the current file name to the largest file name string
-            }
         }
     } // end of while loop
     return largestFileName;
-} // end of "getLargestFileName"
+} // end of "getLargestFileName" function
+
+
+void createDirectory(char* directoryName) {
+
+
+
+
+} // end of "createDirectory" function
+
+
+char* generateDirectoryName() { // this function generates and returns a directory name.
+    char* directoryName = malloc(sizeof(char) * strlen("edwabray.movies.") + 8); // creating the mem. space for the directory name
+    char* directoryNumber = malloc(sizeof(char) * 7); // creating the space to store the random number as a string.
+    int randomNumber = random() % 100000; // generating a random number between 0 and 99999
+    strcpy(directoryName, "edwabray.movies."); // copying the initial name of the directory into the variable
+    sprintf(directoryNumber, "%d", randomNumber); // converting random int into string
+    strcat(directoryName, directoryNumber); // adding random int string into directory name
+    free(directoryNumber);
+    return directoryName;
+} // end of "generateDirectoryName" function
 
 
 void processLargestFile() {
-    char* largestFileName = malloc(sizeof (char) * 255);
-    strcpy(largestFileName, getLargestFileName());
+    char* largestFileName = getLargestFileName();
+    printf("\nNow processing the chosen file named %s \n", largestFileName); // outputing to the user
 
+    char* directoryName = generateDirectoryName(); // using a function to generate the directory name and assigning that to a variable
+    createDirectory(directoryName); // creating the directory
 
+    free(directoryName);
     free(largestFileName);
 } // end of "processLargestFile" function
 
@@ -116,22 +137,18 @@ char* getSpecificFileName() { // This function reads the fileName from the user
 } // end of "getSpecificFileName" function
 
 
-void createDirectory() {
-    printf("Created directory with name ");
-} // end of "createDirectory" function
-
 
 void processSpecificFile () {
     char* fileName = getSpecificFileName(); // calling this function to get the name
     printf("Now processing the chosen file named %s \n", fileName);
 
-    createDirectory();
 
     free(fileName); // freeing the allocated memory
 } // end of "processSpecificFile" function
 
 
 int main(int argc, char **argv) {
+    srandom(time(NULL));
 
     if (getInitialChoice() == 2) { // ending program
         printf("Ending program...");
