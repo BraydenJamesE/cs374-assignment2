81f4ab5012633beae68b3cb54f81d1accf078909
Brayden Edwards <edwabray@oregonstate.edu>
2024-05-01 14:00:20 -0700

    Major updates

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <time.h>
 
 
-
+#define DELIMITER ","
 
 int getInitialChoice() { // this function gets the initial choice from the user. Rather to Select a file to process or to exit the program.
     while (true) { // Continually asking for the right input until the correct one is given.
@@ -93,11 +94,29 @@ char* getLargestFileName() {
 
 
 void createDirectory(char* directoryName) {
-
-
-
-
+    mode_t permissions = 0705; // Permissions gathered from this source: https://jameshfisher.com/2017/02/24/what-is-mode_t/
+    //mkdir(directoryName, permissions); // creating directory
+    printf("Created directory with name %s \n", directoryName);
 } // end of "createDirectory" function
+
+
+char* createFile(char* directoryName, char* year) {
+    mode_t permissions = 0640; // creating the file permissions. Source: https://jameshfisher.com/2017/02/24/what-is-mode_t/
+    char* template = "YYYY.txt";
+    char* fileName = malloc(sizeof(char) * (strlen(template) + 1));
+    size_t filePathSize = sizeof(char) * (strlen(directoryName) + strlen(template) + 2);
+    char* filePath = malloc(sizeof(char) * filePathSize);
+
+    snprintf(fileName, sizeof(char) * (strlen(template) + 1), "%s.txt", year); // creating a string for the fileName
+
+    snprintf(filePath, filePathSize, "%s/%s", directoryName, fileName); // creating the file in the directory.
+//    FILE* file = fopen(filePath, "w"); // creating the file
+//    chmod(filePath, permissions); // setting the permissions of the file
+//    fclose(file); // closing the file
+
+    free(fileName); // freeing the fileName memory
+    return filePath;
+} // end of "createFile" function
 
 
 char* generateDirectoryName() { // this function generates and returns a directory name.
@@ -112,12 +131,80 @@ char* generateDirectoryName() { // this function generates and returns a directo
 } // end of "generateDirectoryName" function
 
 
+void processFiles(char* fileToProcess, char* directoryName) {
+    // read fileToProcess
+    int numberOfYears = 2024 - 1900;
+    char** listOfMovieTitlesPerYear = malloc(numberOfYears * sizeof(char));
+    FILE* fileHandler = fopen(fileToProcess, "r");
+    const int LINE_SIZE = 1001;
+    char fileContent[LINE_SIZE];
+    int index = 0;
+
+    for(int i = 0; i < numberOfYears; i++) {
+        listOfMovieTitlesPerYear[i] = NULL;
+        printf("%d: %s \n", i, listOfMovieTitlesPerYear[i]);
+    }
+
+    while (true) {
+        if (fgets(fileContent, LINE_SIZE, fileHandler) != NULL) { // get the file content
+            if (index == 0) { // ensuring that the header is not read
+                index += 1;
+                continue; // if this is the header, continue in the loop
+            }
+
+            char* movieTitle = malloc(sizeof(char) * 100);
+
+            char* token;
+            char* savePtr = NULL;
+
+            token = strtok_r(fileContent, DELIMITER, &savePtr);
+            strcpy(movieTitle, token);
+            printf("Movie Title: %s \n", movieTitle);
+            token = strtok_r(NULL, DELIMITER, &savePtr); // reading the year
+            printf("year: %s \n", token);
+            char* filePath = createFile(directoryName, token);
+
+            int yearIndex = atoi(token) - 1900;
+
+            if (listOfMovieTitlesPerYear[yearIndex] != NULL) {
+                //int memorySize = sizeof(char) * (strlen(listOfMovieTitlesPerYear[yearIndex]) + strlen(movieTitle) + 2);
+                size_t stringSize = sizeof(char) * (strlen(listOfMovieTitlesPerYear[yearIndex]) + strlen(movieTitle) + 2);
+                listOfMovieTitlesPerYear[yearIndex] = realloc(listOfMovieTitlesPerYear[yearIndex], stringSize);
+                snprintf(listOfMovieTitlesPerYear[yearIndex], stringSize, "%s,%s", listOfMovieTitlesPerYear[yearIndex] ,movieTitle);
+            }
+            else {
+                int memorySize = sizeof(char) * (strlen(movieTitle) + 1);
+                listOfMovieTitlesPerYear[yearIndex] = malloc(memorySize);
+                strcpy(listOfMovieTitlesPerYear[yearIndex], movieTitle);
+            }
+
+
+            token = strtok_r(NULL, DELIMITER, &savePtr); // skipping the language
+            token = strtok_r(NULL, DELIMITER, &savePtr); // skipping the rating
+            free(movieTitle);
+            free(filePath);
+            index += 1;
+        }
+        else { // if file is empty, break the loop
+            break;
+        }
+    } // end of while loop
+
+    for(int i = 0; i < numberOfYears; i++) {
+        printf("%d: %s \n", i, listOfMovieTitlesPerYear[i]);
+    }
+} // end of "processFiles" function
+
+
 void processLargestFile() {
     char* largestFileName = getLargestFileName();
-    printf("\nNow processing the chosen file named %s \n", largestFileName); // outputing to the user
+    printf("\nNow processing the chosen file named %s \n", largestFileName); // outputting to the user
 
     char* directoryName = generateDirectoryName(); // using a function to generate the directory name and assigning that to a variable
     createDirectory(directoryName); // creating the directory
+
+
+    processFiles(largestFileName, directoryName);
 
     free(directoryName);
     free(largestFileName);
